77d98df85063798eef7509c5f60d63e9346435fd
Iago <iagomoreira2353@gmail.com>
2025-09-16 00:22:26 -0300

    FindById Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,12 +11,9 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		Department obj = new Department(1, "books");
-		Seller seller = new Seller(21, "Iago", "iago@gmail.com", new Date(), 3000.0, obj);
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 
-		System.out.println(obj);
+		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 	}
 
